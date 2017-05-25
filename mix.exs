858569617682b5c0d6771ab175d7daa262c8caa7
Clark Kampfe <clark.kampfe@gmail.com>
2017-05-24 20:37:19 -0500

    0.3.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Strand.Mixfile do
 
   def project do
     [app: :strand,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.4",
      package: package(),
      description: "graphs, like Loom",
